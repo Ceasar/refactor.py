4bcebe7a62b39dadb61882eff114ae3ae75a8512
Ceasar Bautista <ceasar.bautista@adroll.com>
2015-05-08 12:52:43 -0700

    Add 'list_dependencies' command

--- refactor/cli.py
+++ refactor/cli.py
@@ -1,3 +1,6 @@
+import ast
+import pprint
+
 import click
 
 import refactor
@@ -11,8 +14,17 @@ def move_node(filename, name):
         click.echo(refactor.move_node(fp, name))
 
 
+@click.command()
+@click.argument('filename')
+def list_dependencies(filename):
+    with open(filename) as fp:
+        deps = refactor.get_dependencies(ast.parse(fp.read()))
+    click.echo(pprint.pformat(deps))
+
+
 @click.group()
 def cli():
     pass
 
 cli.add_command(move_node)
+cli.add_command(list_dependencies)
